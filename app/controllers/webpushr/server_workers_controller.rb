e0132494b05a5d4b71dc9dbe275664fa9c9197dd
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2020-09-08 15:38:45 +0700

    add webpushr/server_workers#index

---
+++ app/controllers/webpushr/server_workers_controller.rb
@@ -0,0 +1,10 @@
+class Webpushr::ServerWorkersController < ApplicationController
+  def index
+    # respond_to do |format|
+    #   format.js
+    #   format.html { redirect_to root_path }
+    # end
+
+    redirect_to root_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root "home#index"
+
+  # get "/webpushr-sw", to: "webpushr/server_workers#index"
 end
